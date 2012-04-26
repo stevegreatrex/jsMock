6a90d0d5329feacc8f46a30333f9528f046dbf9b
stevegreatrex <greatrex696@hotmail.com>
2012-04-26 18:42:06 +0100

    Initial setup and some simple tests for Setup

---
+++ jsMock/Setup.js
@@ -0,0 +1,16 @@
+﻿(function (jsMock) {
+    jsMock.Setup = function (member) {
+        var _self = this,
+            _calls = [],
+            _called = function (arguments) {
+                _calls.push({
+                    arguments: arguments || []
+                });
+            };
+
+        //public members
+        this.member = member;
+        this.calls  = _calls;
+        this.called = _called;
+    };
+})(jsMock);
---
+++ jsMock/tests/TestSetup.js
@@ -0,0 +1,42 @@
+﻿module("Setup test");
+
+test("method name is set", function () {
+    var setup = new jsMock.Setup("member");
+    equal("member", setup.member, "Member should have been set");
+});
+
+test("call count initially 0", function () {
+    var setup = new jsMock.Setup("member");
+
+    equal(0, setup.calls.length, "call count should initially be 0");
+});
+
+test("called increments call count", function () {
+    var setup = new jsMock.Setup("member");
+
+    setup.called();
+
+    equal(1, setup.calls.length, "calls should have been updated");
+});
+
+test("called adds call with empty arguments", function () {
+    var setup = new jsMock.Setup("member");
+
+    setup.called();
+
+    var call = setup.calls[0];
+    deepEqual([], call.arguments, "Arguments should have been set on the call");
+});
+
+
+test("called adds call with populated arguments", function () {
+    var setup = new jsMock.Setup("member");
+
+    var four = { name: "four" };
+    var five = function () { };
+
+    setup.called([1, "2", [3], four, five]);
+
+    var call = setup.calls[0];
+    deepEqual([1, "2", [3], four, five], call.arguments, "Arguments should have been set on the call");
+});
